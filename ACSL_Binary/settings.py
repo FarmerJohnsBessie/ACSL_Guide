fd3a3d57b16aa7de36b7bb16f4bae662e25cd2c7
FarmerJohnsBessie <135478456+FarmerJohnsBessie@users.noreply.github.com>
2023-11-20 11:42:17 -0800

    Update settings.py

--- ACSL_Binary/settings.py
+++ ACSL_Binary/settings.py
@@ -158,5 +158,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'cz52013141834@gmail.com'
-EMAIL_HOST_PASSWORD = 'Tangtang0106!'
+
