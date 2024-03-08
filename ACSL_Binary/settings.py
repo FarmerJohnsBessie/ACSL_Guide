f037dc09efcdd365687c91b8924329aa2938307a
FarmerJohnsBessie <cz52013141834@gmail.com>
2024-03-08 15:39:00 -0800

    adjust dotenv problem

--- ACSL_Binary/settings.py
+++ ACSL_Binary/settings.py
@@ -15,7 +15,6 @@ import django_heroku
 import dj_database_url
 from dotenv import load_dotenv
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
